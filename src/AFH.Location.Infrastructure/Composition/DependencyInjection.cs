71b37cc63f42fa18d0f3960e04e764705d1a475f
ikebotch <ikebotch@gmail.com>
2026-04-04 07:42:33 +0100

    refactor: continue location modularisation

--- src/AFH.Location.Infrastructure/Composition/DependencyInjection.cs
+++ src/AFH.Location.Infrastructure/Composition/DependencyInjection.cs
@@ -78,37 +78,7 @@ public static class DependencyInjection
 
         services.AddScoped<IOfficeRepository, InMemoryOfficeRepository>();
 
-        var policyDbConnectionString = ResolveLocationPolicyDbConnectionString(configuration);
-
-        if (!string.IsNullOrWhiteSpace(policyDbConnectionString))
-        {
-            services.AddDbContext<LocationPolicyDbContext>(options => options.UseSqlServer(policyDbConnectionString));
-            services.AddDbContextFactory<LocationPolicyDbContext>(
-                options => options.UseSqlServer(policyDbConnectionString),
-                ServiceLifetime.Scoped);
-            services.AddAfhCommonErrorsEntityFramework<LocationPolicyDbContext>();
-            services.AddScoped<ICoveragePolicyProvider, SqlCoveragePolicyProvider>();
-            services.AddScoped<IAvailabilityPolicyProvider, SqlAvailabilityPolicyProvider>();
-            services.AddScoped<ISearchAuditRepository, SqlSearchAuditRepository>();
-            services.AddScoped<IAdviserReferenceCacheRepository, SqlAdviserReferenceCacheRepository>();
-            services.AddScoped<IGeoCache, SqlGeoCache>();
-            services.AddScoped<IAdviserGeoCache, SqlGeoCache>();
-            services.AddHostedService<LocationPolicyDbInitializer>();
-        }
-        else
-        {
-            services.AddSingleton<ICoveragePolicyProvider, InMemoryCoveragePolicyProvider>();
-            services.AddSingleton<IAvailabilityPolicyProvider, InMemoryAvailabilityPolicyProvider>();
-            services.AddSingleton<ISearchAuditRepository, NoOpSearchAuditRepository>();
-            services.AddSingleton<IAdviserReferenceCacheRepository, InMemoryAdviserReferenceCacheRepository>();
-            services.AddSingleton<IGeoCache, InMemoryGeoCache>();
-            services.AddSingleton<IAdviserGeoCache, InMemoryAdviserGeoCache>();
-        }
-
-        services.AddSingleton<IRankingPolicyProvider, InMemoryRankingPolicyProvider>();
-        services.AddSingleton<IBaseOfficePolicyProvider, InMemoryBaseOfficePolicyProvider>();
-        services.AddSingleton<IRouteMatrixPolicyProvider, InMemoryRouteMatrixPolicyProvider>();
-        services.AddSingleton<IGeoCachePolicyProvider, InMemoryGeoCachePolicyProvider>();
+        services.AddPolicyStoreModule(configuration);
 
         services.AddMemoryCache();
         services.AddSharePoint(configuration);
---
+++ src/AFH.Location.Infrastructure/Composition/PolicyStoreModuleServiceCollectionExtensions.cs
@@ -0,0 +1,50 @@
+using AFH.Common.Errors.EntityFramework.DependencyInjection;
+using AFH.Location.Application.Abstractions;
+using AFH.Location.Infrastructure.Caching;
+using AFH.Location.Infrastructure.Persistence.PolicyStore;
+using AFH.Location.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AFH.Location.Infrastructure.Composition;
+
+internal static class PolicyStoreModuleServiceCollectionExtensions
+{
+    internal static IServiceCollection AddPolicyStoreModule(this IServiceCollection services, IConfiguration configuration)
+    {
+        var policyDbConnectionString = DependencyInjection.ResolveLocationPolicyDbConnectionString(configuration);
+
+        if (!string.IsNullOrWhiteSpace(policyDbConnectionString))
+        {
+            services.AddDbContext<LocationPolicyDbContext>(options => options.UseSqlServer(policyDbConnectionString));
+            services.AddDbContextFactory<LocationPolicyDbContext>(
+                options => options.UseSqlServer(policyDbConnectionString),
+                ServiceLifetime.Scoped);
+            services.AddAfhCommonErrorsEntityFramework<LocationPolicyDbContext>();
+            services.AddScoped<ICoveragePolicyProvider, SqlCoveragePolicyProvider>();
+            services.AddScoped<IAvailabilityPolicyProvider, SqlAvailabilityPolicyProvider>();
+            services.AddScoped<ISearchAuditRepository, SqlSearchAuditRepository>();
+            services.AddScoped<IAdviserReferenceCacheRepository, SqlAdviserReferenceCacheRepository>();
+            services.AddScoped<IGeoCache, SqlGeoCache>();
+            services.AddScoped<IAdviserGeoCache, SqlGeoCache>();
+            services.AddHostedService<LocationPolicyDbInitializer>();
+        }
+        else
+        {
+            services.AddSingleton<ICoveragePolicyProvider, InMemoryCoveragePolicyProvider>();
+            services.AddSingleton<IAvailabilityPolicyProvider, InMemoryAvailabilityPolicyProvider>();
+            services.AddSingleton<ISearchAuditRepository, NoOpSearchAuditRepository>();
+            services.AddSingleton<IAdviserReferenceCacheRepository, InMemoryAdviserReferenceCacheRepository>();
+            services.AddSingleton<IGeoCache, InMemoryGeoCache>();
+            services.AddSingleton<IAdviserGeoCache, InMemoryAdviserGeoCache>();
+        }
+
+        services.AddSingleton<IRankingPolicyProvider, InMemoryRankingPolicyProvider>();
+        services.AddSingleton<IBaseOfficePolicyProvider, InMemoryBaseOfficePolicyProvider>();
+        services.AddSingleton<IRouteMatrixPolicyProvider, InMemoryRouteMatrixPolicyProvider>();
+        services.AddSingleton<IGeoCachePolicyProvider, InMemoryGeoCachePolicyProvider>();
+
+        return services;
+    }
+}
